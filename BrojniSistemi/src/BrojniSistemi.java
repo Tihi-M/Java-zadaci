40edaa15937bd2edd77cb0f0f9e65ce36f1fc084
Tihi-M <miladinovict025@gmail.com>
2023-10-23 13:07:33 +0200

    Update BrojniSistemi.java

--- BrojniSistemi/src/BrojniSistemi.java
+++ BrojniSistemi/src/BrojniSistemi.java
@@ -4,25 +4,42 @@ import java.util.Scanner;
 import java.lang.Math;
 public class BrojniSistemi {
 	
-	public static boolean provjeriUnos(int bBroj) {
+	public static boolean provjeriUnos(int uneseniBroj, int baza) {
 		int cifra;
-		while(bBroj>1) {
-			cifra = bBroj%10;
-			bBroj/=10;
-			if(cifra != 0 && cifra != 1) {
-				return false;
+		if(baza == 1) {
+			while(uneseniBroj>1) {
+				cifra = uneseniBroj%10;
+				uneseniBroj/=10;
+				if(cifra != 0 && cifra != 1) {
+					return false;
+				}
 			}
+			return true;
+		}
+		else if(baza == 2){
+			while(uneseniBroj>1) {
+				cifra = uneseniBroj%10;
+				uneseniBroj/=10;
+				if(cifra < 0 || cifra > 7) {
+					return false;
+				}
+			}
+			return true;
 		}
 		return true;
 	}
-	public static int izBuD(int binarniBroj) {
+	
+	public static int izXuD(int uneseniBroj, int baza) {
 		int dekadniBr = 0;
 		int stepen = 0;
-		while(binarniBroj>=1) {
-			if(binarniBroj%10 == 1) {
-				dekadniBr += Math.pow(2,stepen);
+		while(uneseniBroj>=1) {
+			if(baza == 1) {
+				dekadniBr += (uneseniBroj%10)*Math.pow(2,stepen);
 			}
-			binarniBroj/=10;
+			else if(baza == 2) {
+				dekadniBr += (uneseniBroj%10)*Math.pow(8,stepen);
+			}
+			uneseniBroj/=10;
 			stepen++;
 		}
 		return dekadniBr;
@@ -30,13 +47,30 @@ public class BrojniSistemi {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Unesite binarni broj: ");
-		int binarniBr = sc.nextInt();
-		while(provjeriUnos(binarniBr)==false) {
-			System.out.println("Unesite binarni broj (0,1): ");
-			binarniBr = sc.nextInt();
+		System.out.println("Izaberite brojni sistem:\n"
+				          +"1. Binarni\n" 
+				          +"2. Oktalni\n"
+				          +"3. Heksadecimalni\n");
+		int brojniSistem = sc.nextInt();
+		while(brojniSistem != 1 && brojniSistem !=2 && brojniSistem!=3 ) {
+			System.out.println("Unesite broj 1 - 3");
+			brojniSistem = sc.nextInt();
 		}
-		System.out.println("U dekakadnom brojnom sistemu " + binarniBr + " je "+ izBuD(binarniBr));
+		
+		
+		System.out.println("Unesite zeljeni broj: ");
+		int uneseniBr = sc.nextInt();
+		while(provjeriUnos(uneseniBr, brojniSistem)==false) {
+			if(brojniSistem == 1) {
+				System.out.println("Unesite binarni broj (0,1): ");
+				uneseniBr = sc.nextInt();
+			}else if(brojniSistem ==2) {
+				System.out.println("Unesite oktalni broj (0-7): ");
+				uneseniBr = sc.nextInt();
+			}
+			
+		}
+		System.out.println("U dekakadnom brojnom sistemu broj " + uneseniBr + " je: "+ izXuD(uneseniBr, brojniSistem));
 		sc.close();
 
 	}
